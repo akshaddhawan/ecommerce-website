ec7fb74e4644fce4f3abe72de75eac4c932f9b52
akshaddhawan <akshaddhawan2@gmail.com>
2024-06-17 15:00:00 +0530

    Define Mongoose model schema for product

---
+++ backend/models/product.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			min: 0,
+			required: true,
+		},
+		image: {
+			type: String,
+			required: [true, "Image is required"],
+		},
+		category: {
+			type: String,
+			required: true,
+		},
+		isFeatured: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
+
+// Code optimization: 2025-10-19T09:16:43.686Z
