c06fb71e5c432b403b5c40c030f1bb19abb2e026
akshaddhawan <akshaddhawan2@gmail.com>
2024-06-27 12:00:00 +0530

    Create and configure main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
+);
+
+// Code optimization: 2025-10-18T11:42:35.054Z
+// Code optimization: 2025-10-19T11:31:17.049Z
