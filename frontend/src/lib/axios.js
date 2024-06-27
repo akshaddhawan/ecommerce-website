61111640587145cac21f9ef59a7bf59802ae0166
akshaddhawan <akshaddhawan2@gmail.com>
2024-06-27 09:00:00 +0530

    Create and configure axios.js

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	withCredentials: true, // send cookies to the server
+});
+
+export default axiosInstance;
+
+// Code optimization: 2025-10-18T08:57:11.510Z
+// Code optimization: 2025-10-19T11:20:18.290Z
