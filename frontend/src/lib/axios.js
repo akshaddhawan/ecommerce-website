0c6b2bead6eb6f989f5b33a78c3eb1a8cf27cfc2
akshaddhawan <akshaddhawan2@gmail.com>
2024-07-03 09:00:00 +0530

    Optimize Tailwind configuration for production builds

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -10,3 +10,4 @@ export default axiosInstance;
 // Code optimization: 2025-10-18T08:57:11.510Z
 // Code optimization: 2025-10-19T11:20:18.290Z
 // Code optimization: 2025-10-17T11:17:45.611Z
+// Code optimization: 2025-10-17T18:28:29.560Z
